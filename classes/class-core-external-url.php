a81b6a52977c0f6f66c790f974579d8aa5c17505
Octavio Martinez <omartinez1618@gmail.com>
2023-12-07 09:58:07 -0400

    set url function

--- classes/class-core-external-url.php
+++ classes/class-core-external-url.php
@@ -11,6 +11,14 @@ class FormExternalUrl {
     }
 
     public static function init() {
+        add_action( "wpforms_ajax_submit_completed", [$this, "connect_url"] );
+    }
+
+    public static function connect_url($form_id) {
+        // Deberiamos tener una zona de configuracion donde asociar el ID con una url
+
+        // Aqui obtenemos los datos del formulario y ya con esto podemos hacer el cURL
+        $form_data = wpforms()->form->get( $form_id, [ 'content_only' => true ] );
 
     }
 
