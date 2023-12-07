9bb40986f89275c3463402edb0bb3833b53eb7da
Octavio Martinez <omartinez1618@gmail.com>
2023-12-07 10:06:41 -0400

    admin menu

--- classes/class-core-external-url.php
+++ classes/class-core-external-url.php
@@ -11,7 +11,32 @@ class FormExternalUrl {
     }
 
     public static function init() {
+        add_action('admin_menu', [$this,'admin_menu']);
         add_action( "wpforms_ajax_submit_completed", [$this, "connect_url"] );
+    }
+
+    public static function admin_menu(){
+        /*
+            Aqui hacemos el diseño de la parte de url, deberia bastar con un 
+            select para el id del formulario
+            y un input para la url
+            se guardan y con el metodo "update_option"
+
+        */
+        add_menu_page(
+            "External Url",
+            "External Url",
+            "manage_options",
+            "external-url",
+            function(){
+                ?>
+                    <div style="margin: 40px;">
+                    </div>
+                <?php
+            },
+            "",
+            6
+        );
     }
 
     public static function connect_url($form_id) {
